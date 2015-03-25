dd6f4d8e996e652a5594967b233fd999c4b3b48a
xtreme-steve-elliott <selliott@pivotal.io>
2015-03-25 17:10:15 -0400

    Update MainActivity.java

--- app/src/main/java/selliott/circularpaging/activities/MainActivity.java
+++ app/src/main/java/selliott/circularpaging/activities/MainActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -28,7 +26,7 @@ public class MainActivity extends ActionBarActivity {
         pager = (CircularViewPager) findViewById(R.id.pager);
         pager.setAdapter(new RotatingPagerAdapter<>(Arrays.asList(generateViewHolder(0), generateViewHolder(1), generateViewHolder(2), generateViewHolder(3), generateViewHolder(4))));
         pager.setOverScrollMode(View.OVER_SCROLL_NEVER);
-        pager.setCurrentItemNoRotate(1, true);
+//        pager.setCurrentItem(0, true);
 //        recyclerView = (RecyclerView) findViewById(R.id.recycler);
 //        recyclerView.setLayoutManager(new SnappingLinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 //        recyclerView.setAdapter(new CircularRecyclerAdapter());
@@ -49,23 +47,5 @@ public class MainActivity extends ActionBarActivity {
                 return v;
             }
         };
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
